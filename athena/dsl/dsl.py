8495f8b8e9c2465d2c85b081d26f17b8b9a76c1c
Ramsay Taylor <R.Taylor@dcs.shef.ac.uk>
2013-11-10 15:47:26 +0000

    Updated the DSL and added the implies method + tests.

--- athena/dsl/dsl.py
+++ athena/dsl/dsl.py
@@ -6,10 +6,10 @@ class VariableNotFoundException(Exception):
     def __str__(self):
         return "Varname: " + str(self.value)
 
-class Exp:
+class Exp(object):
     pass
 
-class BinOp:
+class BinOp(Exp):
     def __init__(self,left,right):
         self.left = left
         self.right = right
@@ -17,7 +17,35 @@ class BinOp:
     def __str__(self):
         return str(self.left) + " " + self.opimage + " " + str(self.right)
 
-class MonOp:
+    def is_commutative(self):
+        # Binary operators are not commutative by default,
+        # this method should be overriden for those that are.
+        return False
+
+    def implies(self,other):
+        if isinstance(other,self.__class__):
+            if self.is_commutative():
+                return (
+                    (
+                        self.left.implies(other.left)
+                        and
+                        self.right.implies(other.right)
+                    ) or (
+                        self.left.implies(other.right)
+                        and
+                        self.right.implies(other.left)
+                    )
+                )
+            else:
+                return (
+                    self.left.implies(other.left)
+                    and
+                    self.right.implies(other.right)
+                )
+        else:
+            return False
+
+class MonOp(Exp):
     def __init__(self,operand):
         self.operand = operand
 
@@ -28,36 +56,102 @@ class Not(MonOp):
     opimage = "not"
     def ev(self,vs):
         return not self.operand.ev(vs)
+    def implies(self,other):
+        if isinstance(other,Not):
+            return self.operand.implies(other.operand)
+        elif isinstance(self.operand,Not):
+            return self.operand.operand.implies(other)
+        else:
+            return False
 
 class Equals(BinOp):
     opimage = "="
     def ev(self,vs):
         return self.left.ev(vs) == self.right.ev(vs)
+    def is_commutative(self):
+        return True
+    def implies(self,other):
+        if (isinstance(other,Equals) 
+            or isinstance(other,GEQ)
+            or isinstance(other,LEQ)):
+            return (
+                (self.left.implies(other.left)
+                 and
+                 self.right.implies(other.right)
+             ) or (
+                 # Equals is commutative...
+                 self.left.implies(other.right)
+                 and
+                 self.right.implies(other.left)
+             )
+            )
+        else:
+            return False
 
 class LessThan(BinOp):
     opimage = "<"
     def ev(self,vs):
         return self.left.ev(vs) < self.right.ev(vs)
 
+
 class GreaterThan(BinOp):
     opimage = ">"
     def ev(self,vs):
         return self.left.ev(vs) > self.right.ev(vs)
+        
 
 class LEQ(BinOp):
     opimage = "<="
     def ev(self,vs):
         return self.left.ev(vs) <= self.right.ev(vs)
+    def implies(self,other):
+        if isinstance(other,LessThan):
+            return (self.left.implies(other.left)
+                    and
+                    self.right.implies(other.right))
+        else:
+            return super(LEQ,self).implies(other)
 
 class GEQ(BinOp):
     opimage = ">="
     def ev(self,vs):
         return self.left.ev(vs) >= self.right.ev(vs)
+    def implies(self,other):
+        if isinstance(other,LessThan):
+            return (self.left.implies(other.left)
+                    and
+                    self.right.implies(other.right))
+        else:
+            return super(GEQ,self).implies(other)
 
 class NEQ(BinOp):
     opimage = "!="
     def ev(self,vs):
         return self.left.ev(vs) != self.right.ev(vs)
+    def is_commutative(self):
+        return True
+    def implies(self,other):
+        if isinstance(other,NEQ):
+            return super(NEQ,self).implies(other)
+        else:
+            if isinstance(other,Not):
+                if isinstance(other.operand,Equals):
+                    return (
+                        (
+                            self.left.implies(other.operand.left)
+                            and
+                            self.right.implies(other.operand.right)
+                        ) or (
+                            # NEQ is commutative
+                            self.left.implies(other.operand.right)
+                            and
+                            self.right.implies(other.oeprand.left)
+                        )
+                    )
+                else:
+                    return False
+            else:
+                return False
 
 class Lit(Exp):
     def __init__(self,val):
@@ -69,15 +163,34 @@ class Lit(Exp):
     def __str__(self):
         return str(self.val)
 
+    def implies(self,other):
+        try:
+            return self.val == other.ev({})
+        except VariableNotFoundException:
+            return False
+
 class Plus(BinOp):
     opimage = "+"
     def ev(self,vs):
         return self.left.ev(vs) + self.right.ev(vs)
+    def is_commutative(self):
+        return True
+    def implies(self,other):
+        try:
+            self.ev({}) == other.ev({})
+        except VariableNotFoundException:
+            return super(Plus,self).implies(other)
 
 class Minus(BinOp):
     opimage = "-"
     def ev(self,vs):
         return self.left.ev(vs) - self.right.ev(vs)
+
+    def implies(self,other):
+        try:
+            self.ev({}) == other.ev({})
+        except VariableNotFoundException:
+            return super(Minus,self).implies(other)
 
 class Var(Exp):
     def __init__(self,varname):
@@ -92,6 +205,12 @@ class Var(Exp):
     def __str__(self):
         return self.varname
 
+    def implies(self,other):
+        if isinstance(other,Var):
+            return self.varname == other.varname
+        else:
+            return False
+
 class Concat(BinOp):
     def __str__(self):
         return str(self.left) + str(self.right)
@@ -99,7 +218,22 @@ class Concat(BinOp):
     def ev(self,vs):
         return str(self.left.ev(vs)) + str(self.right.ev(vs))
 
+    def implies(self,other):
+        try:
+            self.ev({}) == other.ev({})
+        except VariableNotFoundException:
+            return super(Concat,self).implies(other)
+
+
 class Wild(Exp):
     def __str__(self):
         return "<*>"
-
+    def implies(self,other):
+        if (isinstance(other,Wild)
+            or isinstance(other,Concat)
+            or isinstance(other,Var)
+            or isinstance(other,Lit)
+            ):
+            return True
+        else:
+            return False
--- test/dsl/testdsl.py
+++ test/dsl/testdsl.py
@@ -9,6 +9,10 @@ class TestDSL(unittest.TestCase):
         self.assertEqual(Lit('1').ev([]),'1')
     def test_lit_string(self):
         self.assertEqual(str(Lit(1)), '1')
+    def test_lit_imlies(self):
+        self.assertTrue(Lit(1).implies(Lit(1)))
+        self.assertFalse(Lit(1).implies(Lit(2)))
+        self.assertFalse(Lit(1).implies(Var('s')))
 
     def test_var_works(self):
         self.assertEqual(Var('a').ev({'a':1}),1)
@@ -17,6 +21,10 @@ class TestDSL(unittest.TestCase):
             Var('a').ev({'b':22})
     def test_var_string(self):
         self.assertEqual(str(Var('a')),'a')
+    def test_var_implies(self):
+        self.assertTrue(Var('x').implies(Var('x')))
+        self.assertFalse(Var('x').implies(Var('s')))
+        self.assertFalse(Var('x').implies(GEQ(Lit(1),Lit(0))))
 
     def test_eq(self):
         self.assertEqual(Equals(Lit(1),Lit(1)).ev([]),True)
@@ -24,6 +32,14 @@ class TestDSL(unittest.TestCase):
         self.assertEqual(Equals(Lit(1),Lit(2)).ev([]),False)
     def test_eq_string(self):
         self.assertEqual(str(Equals(Lit(1),Lit(2))),"1 = 2")
+    def test_eq_implies(self):
+        self.assertTrue(Equals(Lit(1),Lit(1)).implies(Equals(Lit(1),Lit(1))))
+        self.assertFalse(Equals(Lit(1),Lit(1)).implies(Equals(Lit(47),Lit(47))))
+        self.assertTrue(Equals(Var('x'),Var('y')).implies(Equals(Var('y'),Var('x'))))
+        self.assertTrue(Equals(Var('x'),Var('y')).implies(LEQ(Var('x'),Var('y'))))
+        self.assertTrue(Equals(Var('x'),Var('y')).implies(GEQ(Var('x'),Var('y'))))
+        self.assertTrue(Equals(Var('x'),Var('y')).implies(LEQ(Var('y'),Var('x'))))
+        self.assertTrue(Equals(Var('x'),Var('y')).implies(GEQ(Var('y'),Var('x'))))
 
     def test_neq(self):
         self.assertEqual(NEQ(Lit(1),Lit(2)).ev([]),True)
@@ -31,7 +47,11 @@ class TestDSL(unittest.TestCase):
         self.assertEqual(NEQ(Lit(1),Lit(1)).ev([]),False)
     def test_neq_string(self):
         self.assertEqual(str(NEQ(Lit(1),Lit(1))),"1 != 1")
-
+    def test_neq_implies(self):
+        self.assertTrue(NEQ(Var('x'),Var('y')).implies(NEQ(Var('x'),Var('y'))))
+        self.assertFalse(NEQ(Var('x'),Var('y')).implies(Equals(Var('x'),Var('y'))))
+        self.assertFalse(NEQ(Var('x'),Var('y')).implies(NEQ(Var('x'),Var('z'))))
+        self.assertTrue(NEQ(Var('x'),Var('y')).implies(NEQ(Var('y'),Var('x'))))
 
     def test_lessthan(self):
         self.assertEqual(LessThan(Lit(1),Lit(2)).ev([]),True)
@@ -39,6 +59,9 @@ class TestDSL(unittest.TestCase):
         self.assertEqual(LessThan(Lit(1),Lit(1)).ev([]),False)
     def test_lessthan_string(self):
         self.assertEqual(str(LessThan(Lit(1),Lit(1))),"1 < 1")
+    def test_lessthan_implies(self):
+        self.assertTrue(LessThan(Var('x'),Var('y')).implies(LessThan(Var('x'),Var('y'))))
+        self.assertFalse(LessThan(Var('x'),Var('y')).implies(LessThan(Var('y'),Var('x'))))
 
     def test_GreaterThan(self):
         self.assertEqual(GreaterThan(Lit(2),Lit(1)).ev([]),True)
@@ -46,6 +69,9 @@ class TestDSL(unittest.TestCase):
         self.assertEqual(GreaterThan(Lit(1),Lit(2)).ev([]),False)
     def test_GreaterThan_string(self):
         self.assertEqual(str(GreaterThan(Lit(1),Lit(2))),"1 > 2")
+    def test_GreaterThan_implies(self):
+        self.assertTrue(GreaterThan(Var('x'),Var('y')).implies(GreaterThan(Var('x'),Var('y'))))
+        self.assertFalse(GreaterThan(Var('x'),Var('y')).implies(GreaterThan(Var('y'),Var('x'))))
 
     def test_LEQ(self):
         self.assertEqual(LEQ(Lit(1),Lit(2)).ev([]),True)
@@ -55,6 +81,10 @@ class TestDSL(unittest.TestCase):
         self.assertEqual(LEQ(Lit(2),Lit(1)).ev([]),False)
     def test_LEQ_string(self):
         self.assertEqual(str(LEQ(Lit(2),Lit(1))),"2 <= 1")
+    def test_LEQ_implies(self):
+        self.assertTrue(LEQ(Var('x'),Var('y')).implies(LEQ(Var('x'),Var('y'))))
+        self.assertFalse(LEQ(Var('x'),Var('y')).implies(LEQ(Var('y'),Var('x'))))
+        self.assertFalse(LEQ(Var('x'),Var('y')).implies(Equals(Var('x'),Var('y'))))
 
     def test_GEQ(self):
         self.assertEqual(GEQ(Lit(2),Lit(1)).ev([]),True)
@@ -69,10 +99,21 @@ class TestDSL(unittest.TestCase):
         self.assertEqual(Plus(Lit(1),Lit(1)).ev([]),2)
     def test_Plus_string(self):
         self.assertEqual(str(Plus(Lit(1),Lit(1))),"1 + 1")
+    def test_Plus_implies(self):
+        self.assertTrue(Plus(Lit(1),Var('x')).implies(Plus(Lit(1),Var('x'))))
+        self.assertTrue(Plus(Lit(1),Var('x')).implies(Plus(Var('x'),Lit(1))))
+        self.assertTrue(Plus(Var('x'),Lit(1)).implies(Plus(Var('x'),Minus(Lit(2),Lit(1)))))
+        # This requires some simplification
+        # FIXME implement later?
+        #self.assertTrue(Plus(Var('x'),Lit(1)).implies(Minus(Lit(2),Plus(Var('x'),Lit(1)))))
+
     def test_Minus(self):
         self.assertEqual(Minus(Lit(1),Lit(1)).ev([]),0)
     def test_Minus_string(self):
         self.assertEqual(str(Minus(Lit(1),Lit(1))),"1 - 1")
+    def test_Minus_implies(self):
+        self.assertTrue(Minus(Lit(1),Var('x')).implies(Minus(Lit(1),Var('x'))))
+        self.assertFalse(Minus(Lit(1),Var('x')).implies(Minus(Var('x'),Lit(1))))
 
     def test_concat(self):
         self.assertEqual(Concat(Lit("key="),Lit("abc")).ev([]),"key=abc")
