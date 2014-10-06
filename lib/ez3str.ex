c95d8f700036d1b61e98bef05db456541cee0980
Ramsay Taylor <r.taylor@dcs.shef.ac.uk>
2014-10-06 07:16:03 +0100

    Z3-str interface with tests.

--- lib/ez3str.ex
+++ lib/ez3str.ex
@@ -3,41 +3,62 @@ defmodule EZ3Str do
 		z3cmd = Application.get_env(:athena, :z3cmd)
 		tmpfolder = System.tmp_dir()
 		tfile = Path.join([tmpfolder,"athena_z3_str.z3str"])
+		:io.format("~p~n",[tfile])
 		File.write(tfile, statementstring)
 		{result,exitval} = System.cmd(z3cmd,["-f",tfile],[])
-		IO.puts result
 		case exitval do
-			0 -> {:ok, parse_result(result)}
+			0 -> parse_result(result)
 			_ -> {:error,exitval}
 		end
 	end
 
 	defp parse_result(result) do
 		lines = String.split(result,"\n")
-		List.foldl(lines,%{},&parse_result_line/2)
+		parse_result_lines(lines,%{})
 	end
 
-	defp parse_result_line(line,vars) do
-		if String.contains?(line,":") do
-			[name,content] = String.split(line,":")
-			arrows = String.split(content, "->")
-			key = String.to_atom(String.strip(name))
-			val = parse_val(List.last(arrows))
-			Map.put(vars,key,val)
-		else
-			vars
+	defp parse_result_lines([],vars) do
+		vars
+	end
+	defp parse_result_lines([line|lines],vars) do
+		cond do
+			String.contains?(line,"(error") ->
+				[_,msg,_] = String.split(line,"\"")
+				Map.put(Map.put(vars,:error_msg, msg), :SAT, :error)
+			String.contains?(line,":") ->
+				[name,content] = String.split(line,":")
+				arrows = String.split(content, "->")
+				key = String.to_atom(String.strip(name))
+				val = parse_val(List.last(arrows))
+				parse_result_lines(lines,Map.put(vars,key,val))
+			String.contains?(line,">>") ->
+				sat = String.strip(String.strip(line, ?>))
+				cond do
+					sat == "UNSAT" ->
+						parse_result_lines(lines,Map.put(vars,:SAT,false))
+					sat == "SAT" ->
+						parse_result_lines(lines,Map.put(vars,:SAT,true))
+					sat == "UNKNOWN" ->
+						parse_result_lines(lines,Map.put(vars,:SAT,:unknown))
+					true ->
+						parse_result_lines(lines,Map.put(vars,:SAT,:error))
+				end
+			true ->
+				parse_result_lines(lines,vars)
 		end
 	end
 
 	defp parse_val(vstring) do
 		stripped = String.strip(vstring)
 		if String.starts_with?(stripped,"\"") do
-				String.strip(stripped,?\")
+			String.strip(stripped,?\")
 		else
-			{i,rest} = Integer.parse(stripped)
-			if rest == "" do
-				i
-			else
+			case Integer.parse(stripped) do
+			{i,""} -> 
+					i
+			{_,<< "." , _>>} ->
+				String.to_float(stripped)
+			_ ->
 				stripped
 			end
 		end
--- test/ez3str_test.exs
+++ test/ez3str_test.exs
@@ -1,13 +1,39 @@
 defmodule EZ3StrTest do
   use ExUnit.Case
 
-  test "Executes" do
+  test "SAT" do
     assert EZ3Str.runZ3Str("(declare-variable p11 String)
 (assert (= p11 \"wibble\"))
 (declare-variable i Int)
 (assert (= i 42))
-") == {:ok, %{:p11 => "wibble", :i => 42}}
+") == %{:SAT => true, :p11 => "wibble", :i => 42}
   end
 
+	test "UNSAT" do
+		assert EZ3Str.runZ3Str("(declare-variable p11 String)
+(assert (= p11 \"wibble\"))
+(declare-variable i Int)
+(assert (= i 42))
+(assert (= i 44))
+") == %{:SAT => false}
+		end
+
+	test "Error" do
+		assert EZ3Str.runZ3Str("(declare-variable p11 String)
+(assert (= p11 \"wibble\"))
+(declare-variable i Int)
+(assert (= i 42))
+((nonsensenobrackets
+") == %{:SAT => :error, error_msg: "line 8 column 1: invalid command, symbol expected"}
+		end
+
+	test "Unknown" do
+		assert EZ3Str.runZ3Str("(declare-variable p11 String)
+(assert (= p11 \"wibble\"))
+(declare-variable i Int)
+(assert (= i 42))
+(assert (forall ((s String)) (= p11 s))) 
+") == %{:SAT => :unknown}
+		end
 
 end
